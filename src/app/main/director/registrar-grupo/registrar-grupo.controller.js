3202b9282f72c2fd123a07420cebeda59c0767a1
gerardogsp21 <gerardogsp21@gmail.com>
2016-05-14 18:12:44 -0500

    Registra Grupos de investigación

---
+++ src/app/main/director/registrar-grupo/registrar-grupo.controller.js
@@ -0,0 +1,42 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app.director.registrarGrupo')
+        .controller('RegistrarGrupoController', RegistrarGrupoController);
+
+
+    /** @ngInject */
+    function RegistrarGrupoController(Restangular, $mdToast) {
+        var vm = this;
+        vm.grupo = {};
+        var grupo = Restangular.all('/grupo');
+
+
+        vm.guardar = guardar;
+        vm.limpiar = limpiar;
+
+        function guardar(){
+            vm.grupo.usuario_id = JSON.parse(sessionStorage.usuario).id;
+            grupo.post(vm.grupo).then(function (d) {
+                message(d);
+                vm.grupo = '';
+            }), function (error) {
+                var mensajeError = error.status == 401 ? error.data.mensajeError : 'A ocurrido un error inesperado';
+                message(mensajeError);
+            }
+        }
+
+        function limpiar() {
+            vm.director = '';
+        }
+        function message(body) {
+            $mdToast.show({
+                template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
+                hideDelay: 3000,
+                position : 'top right',
+                parent   : '#content'
+            });
+        }
+    }
+})();
