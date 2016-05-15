ef7ea5d1ca11a104bd0fcdbe479d295606945326
RicardoKaro <ricardo.caro9@hotmail.com>
2016-05-15 15:48:01 -0500

    abre las convocatorias falta la validacion de fechas

--- src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
+++ src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
@@ -7,22 +7,31 @@
 
 
     /** @ngInject */
-    function AbrirConvocatoriaController() {
+    function AbrirConvocatoriaController(Restangular) {
         var vm = this;
         vm.convocatoria = {};
-        var convovatorias = [];
-
+        var convocatoria = Restangular.all('/convocatorias');
 
         vm.guardar = guardar;
 
         function guardar() {
-            if (convovatorias.push(vm.convocatoria)) {
-                alert('se registro la convocatoria correctamente');
-                vm.convocatoria = {};
-                console.log(convovatorias);
-            } else {
-                alert('no se pudo registrar la convocatoria');
+            vm.convocatoria.usuario_id = JSON.parse(sessionStorage.usuario).id;
+            convocatoria.post(vm.convocatoria).then(function (d) {
+                message(d);
+                vm.convocatoria = '';
+            }), function (error) {
+                var mensajeError = error.status == 401 ? error.data.mensajeError : 'A ocurrido un error inesperado';
+                message(mensajeError);
             }
+        };
+
+        function message(body) {
+            $mdToast.show({
+                template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
+                hideDelay: 3000,
+                position : 'top right',
+                parent   : '#content'
+            });
         }
     }
 })();
