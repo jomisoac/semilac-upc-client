200d9008e291c76d06c944f8e7319ad3d8d2dab0
RicardoKaro <ricardo.caro9@hotmail.com>
2016-05-21 16:36:28 -0500

    envia el formato acta tipo file de la parte del cliente al servidor

--- src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
+++ src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
@@ -7,31 +7,56 @@
 
 
     /** @ngInject */
-    function AbrirConvocatoriaController(Restangular, $mdToast) {
+    function AbrirConvocatoriaController(Restangular, $mdToast, $state, $stateParams, $timeout) {
         var vm = this;
         vm.convocatoria = {};
         var convocatoria = Restangular.all('/convocatorias');
 
         vm.guardar = guardar;
 
-        function guardar() {            
+        function guardar() {
             vm.convocatoria.usuario_id = JSON.parse(sessionStorage.usuario).id;
             console.log(vm.convocatoria.acta);
-            convocatoria.post(vm.convocatoria).then(function (d) {
-                message(d);
-                vm.convocatoria = '';
-            }), function (error) {
-                var mensajeError = error.status == 401 ? error.data.mensajeError : 'A ocurrido un error inesperado';
-                message(mensajeError);
-            }
-        };
+            convocatoria.post(vm.convocatoria)
+                .then(
+                    function (d) {
+                        if (vm.convocatoria.acta) {
+                            var formData = new FormData();
+                            formData.append("acta", vm.convocatoria.acta);
+                            Restangular.all('/convocatorias/' + d.convocatoria.id + '/acta')
+                                .withHttpConfig({transformRequest: angular.identity})
+                                .customPOST(formData, undefined, undefined,
+                                    {'Content-Type': undefined})
+                                .then(function (respuestaActa) {
+                                        message(d.mensaje);
+                                        $timeout(function () {
+                                            $state.transitionTo($state.current, $stateParams, {
+                                                reload: true,
+                                                inherit: false,
+                                                notify: true
+                                            });
+                                        }, 3000);
+                                    },
+                                    function (error) {
+                                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
+                                        message(mensajeError);
+                                    }
+                                );
+                        }
+                    },
+                    function (error) {
+                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
+                        message(mensajeError);
+                    }
+                );
+        }
 
         function message(body) {
             $mdToast.show({
-                template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
+                template: '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
                 hideDelay: 3000,
-                position : 'top right',
-                parent   : '#content'
+                position: 'top right',
+                parent: '#content'
             });
         }
     }
