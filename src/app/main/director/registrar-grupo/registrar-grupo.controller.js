582970d1887b13b4bd580185573db09f8fbaf3a8
Andrés Herrera García <andreshg112@gmail.com>
2016-05-22 02:16:15 -0500

    Se corrigieron errores reportados en trello.

--- src/app/main/director/registrar-grupo/registrar-grupo.controller.js
+++ src/app/main/director/registrar-grupo/registrar-grupo.controller.js
@@ -16,26 +16,29 @@
         vm.guardar = guardar;
         vm.limpiar = limpiar;
 
-        function guardar(){
-            vm.grupo.usuario_id = JSON.parse(sessionStorage.usuario).id;
-            grupo.post(vm.grupo).then(function (d) {
-                message(d);
-                vm.grupo = '';
-            }), function (error) {
-                var mensajeError = error.status == 401 ? error.data.mensajeError : 'A ocurrido un error inesperado';
-                message(mensajeError);
-            }
+        function guardar() {
+            vm.grupo.usuario_id = JSON.parse(sessionStorage.usuario).usuario_id;
+            grupo.post(vm.grupo).then(
+                function (d) {
+                    message(d);
+                    vm.grupo = {};
+                },
+                function (error) {
+                    var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
+                    message(mensajeError);
+                }
+            );
         }
 
         function limpiar() {
-            vm.director = '';
+            vm.director = {};
         }
         function message(body) {
             $mdToast.show({
-                template : '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
+                template: '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
                 hideDelay: 3000,
-                position : 'top right',
-                parent   : '#content'
+                position: 'top right',
+                parent: '#content'
             });
         }
     }
