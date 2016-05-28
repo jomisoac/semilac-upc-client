dbfeb09f30c59381fbbf005ea94b29489d093aad
Andrés Herrera García <andreshg112@gmail.com>
2016-05-28 12:10:01 -0500

    Se quitó la carpeta registrar-tutor que no se está usando.

--- src/app/main/tutor/tutor.module.js
+++ src/app/main/tutor/tutor.module.js
@@ -3,7 +3,6 @@
 
     angular
         .module('app.tutor', [
-            'app.tutor.registrarTutor',
             'app.tutor.registrarSemillero',
             'app.tutor.registrarInvitacion'
         ])
@@ -15,41 +14,17 @@
             title: 'TUTOR',
             group: true,
             weight: 1,
-            hidden: function(){
+            hidden: function () {
                 return authProvider.checkUser(['TUTOR']);
             }
         });
-        // msNavigationServiceProvider.saveItem('tutor.gestionar_tutor', {
-        //     title: 'Gestionar tutor',
-        //     icon: 'icon-tile-four',
-        //     weight: 1,
-        //     hidden: function(){
-        //         return authProvider.checkUser(['TUTOR']);
-        //     }
-        // });
-        //
-        // msNavigationServiceProvider.saveItem('tutor.gestionar_tutor.registrar', {
-        //     title: 'Registrar tutor',
-        //     state: 'app.registrar_tutor',
-        //     hidden: function(){
-        //         return authProvider.checkUser(['TUTOR']);
-        //     }
-        // });
 
-        // msNavigationServiceProvider.saveItem('tutor.gestionar_tutor.consultar', {
-        //     title: 'Consultar tutor',
-        //     state: 'app.consultar-tutor',
-        //     hidden: function(){
-        //         return authProvider.checkUser(['TUTOR']);
-        //     }
-        // });
-        
         msNavigationServiceProvider.saveItem('tutor.registrar-semillero', {
             title: 'Semilleros de investigacion',
             icon: 'icon-tile-four',
             state: 'app.registrar_semillero',
             weight: 1,
-            hidden: function(){
+            hidden: function () {
                 return authProvider.checkUser(['TUTOR']);
             }
         });
