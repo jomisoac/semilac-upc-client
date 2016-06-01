f1706b7914d183418a790e671b300cff524d1f94
Andrés Herrera García <andreshg112@gmail.com>
2016-06-01 01:06:24 -0500

    Corregido y validando las fechas de las convocatorias.

--- src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
+++ src/app/main/director/abrir-convocatoria/abrir-convocatoria.controller.js
@@ -8,25 +8,39 @@
 
     /** @ngInject */
     function AbrirConvocatoriaController(Restangular, $mdToast, $state, $stateParams, $timeout, authService) {
+        //Variables privadas
         var vm = this;
-        vm.convocatoria = {};
         var convocatoria = Restangular.all('/convocatorias');
 
-        vm.fechaValida = fechaValida;
+        //Variables y funciones públicas
+        vm.cambioFechaFinal = cambioFechaFinal;
+        vm.cambioFechaInicial = cambioFechaInicial;
         vm.guardar = guardar;
-        
-        function fechaValida() {
-            if(vm.convocatoria.fechainicial.getFullYear() <= vm.convocatoria.fechafinal.getFullYear()){
-                if(vm.convocatoria.fechainicial.getMonth() <= vm.convocatoria.fechafinal.getMonth() ){
-                    if(vm.convocatoria.fechainicial.getDate() < vm.convocatoria.fechafinal.getDate()){
-                    }else{
-                        alert("la fecha final debe ser mayo que la fecha inicial...");
-                    }
-                }else{
-                    alert("la fecha final debe ser mayo que la fecha inicial...");
-                }
-            }else{
-                alert("la fecha final debe ser mayo que la fecha inicial...");
+
+        //Declaración de funciones
+        function activate() {
+            vm.convocatoria = {};
+            var hoy = new Date();
+            //La fecha inicial mínima siempre será el día actual...
+            //...porque no se puede abrir convocatorias para días que ya pasaron.
+            vm.fechaInicialMin = hoy;
+            vm.fechaFinalMin = hoy;
+        }
+
+        function cambioFechaInicial() {
+            //La fecha final de la convocatoria debe ser como mínimo el mismo día en que se abre la convocatoria, es decir, la convocatoria cierra el mismo día.
+            if (vm.convocatoria.fechainicial) {
+                vm.fechaFinalMin = vm.convocatoria.fechainicial;
+            } else {
+                vm.fechaFinalMin = hoy;
+            }
+        }
+
+        function cambioFechaFinal() {
+            if (vm.convocatoria.fechafinal) {
+                vm.fechaInicialMax = vm.convocatoria.fechafinal;
+            } else {
+                vm.fechaInicialMax = undefined;
             }
         }
 
@@ -36,35 +50,35 @@
             vm.convocatoria.usuario_id = authService.currentUser().usuario_id
             convocatoria.post(vm.convocatoria)
                 .then(
-                    function (d) {
-                        if (vm.convocatoria.acta) {
-                            var formData = new FormData();
-                            formData.append("acta", vm.convocatoria.acta);
-                            Restangular.all('/convocatorias/' + d.convocatoria.id + '/acta')
-                                .withHttpConfig({transformRequest: angular.identity})
-                                .customPOST(formData, undefined, undefined,
-                                    {'Content-Type': undefined})
-                                .then(function (respuestaActa) {
-                                        message(d.mensaje);
-                                        $timeout(function () {
-                                            $state.transitionTo($state.current, $stateParams, {
-                                                reload: true,
-                                                inherit: false,
-                                                notify: true
-                                            });
-                                        }, 3000);
-                                    },
-                                    function (error) {
-                                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
-                                        message(mensajeError);
-                                    }
-                                );
-                        }
-                    },
-                    function (error) {
-                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
-                        message(mensajeError);
+                function (d) {
+                    if (vm.convocatoria.acta) {
+                        var formData = new FormData();
+                        formData.append("acta", vm.convocatoria.acta);
+                        Restangular.all('/convocatorias/' + d.convocatoria.id + '/acta')
+                            .withHttpConfig({ transformRequest: angular.identity })
+                            .customPOST(formData, undefined, undefined,
+                            { 'Content-Type': undefined })
+                            .then(function (respuestaActa) {
+                                message(d.mensaje);
+                                $timeout(function () {
+                                    $state.transitionTo($state.current, $stateParams, {
+                                        reload: true,
+                                        inherit: false,
+                                        notify: true
+                                    });
+                                }, 3000);
+                            },
+                            function (error) {
+                                var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
+                                message(mensajeError);
+                            }
+                            );
                     }
+                },
+                function (error) {
+                    var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
+                    message(mensajeError);
+                }
                 );
         }
 
@@ -76,5 +90,8 @@
                 parent: '#content'
             });
         }
+
+        //Llamadas a las funciones
+        activate();
     }
 })();
