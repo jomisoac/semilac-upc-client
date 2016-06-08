4b46a2d388fda0b0462fc0afa1b34aae4c3ca4d4
martinr325 <martinromero325@gmail.com>
2016-06-07 20:42:09 -0500

    creando  historia 12  Como tutor quiero solicitar al director del comité de investigación el aval para que la universidad certifique el semillero de investigación.(con errores) nota:andres la terminara

--- src/app/main/tutor/solicitud-aval-convocatoria/solicitud-aval-convocatoria.controller.js
+++ src/app/main/tutor/solicitud-aval-convocatoria/solicitud-aval-convocatoria.controller.js
@@ -9,47 +9,45 @@
     function SolicitudAvalConvocatoriaController(Restangular, $mdToast, authService) {
         //console.log("RegistrarInvitacionController");
         var vm = this;
-       // vm.semillero = {};
-        //vm.semilleros = [];
-       // vm.convocatoria = {};
-       // vm.convocatorias = [];
-        vm.solicitud_aval = {};
+        vm.semillero = {};
+        vm.semilleros = [];
+        vm.convocatoriaAbierta = {};
         vm.solicitudes_aval = [];
-        vm.respuesta = 'enviar';
         vm.enviar = enviar;
         vm.limpiar = limpiar;
 
-        var solicitudes = Restangular.all('/solicitud-aval-convocatoria');
-        var convocatorias = Restangular.all('/convocatorias/convocatoria-abierta').getList().$object;
 
-        function enviar() {
+        var solicitudes = Restangular.all('/solicitud-aval-convocatoria');
+        var convocatoriaAbierta = Restangular.one('/convocatorias/convocatoria-abierta').$object;
+        vm.convocatoriaAbierta = convocatoriaAbierta;
+        cargarSemilleros();
+        function cargarSemilleros() {
+            var tutor_id = authService.currentUser().datos.id;
+            vm.semilleros = Restangular.all('/tutores/' + tutor_id + '/semilleros').getList().$object;
+        }
+
+        // cargarConvocatoria();
+        //function cargarConvocatoria() {
+        //   vm.convocatorias = convocatorias;
+        // }
+
+        function enviar(form) {
             var solicitud = {
-                'convocatoria_id': vm.convocatoria_id_seleccionado,
+                'convocatoria_id': 1,
                 'semillero_id': vm.semillero_id_seleccionado
             };
+
             solicitudes.post(solicitud).then(
                     function (d) {
                         message(d.mensaje);
                         solicitud = {};
                     },
                     function (error) {
-                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error inesperado.';
-
+                        var mensajeError = error.status == 401 ? error.data.mensajeError : 'Ha ocurrido un error                    inesperado.';
                     }
             );
         }
 
-        cargarSemilleros();
-        function cargarSemilleros() {
-            var tutor_id = authService.currentUser().datos.id;
-            vm.semilleros = Restangular.all('/tutores/' + tutor_id + '/semilleros').getList().$object;
-        }
-        
-        cargarConvocatorias();
-        function cargarConvocatorias(){
-            vm.convocatoria= convocatorias;
-        }
-        
         function message(body) {
             $mdToast.show({
                 template: '<md-toast id="language-message" layout="column" layout-align="center start"><div class="md-toast-content">' + body + '</div></md-toast>',
